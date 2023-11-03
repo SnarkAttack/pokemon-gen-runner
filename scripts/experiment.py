f134b3e42d87a5f60e7b8eb132f273fcd3e8f427
Patrick McQuay <patrick@patricks-mbp.myfiosgateway.com>
2023-11-03 19:52:53 -0400

    Commit experimentation script for some simple usage

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,5 +1,22 @@
-from pyboy import PyBoy
+from pyboy import PyBoy, WindowEvent
 
 if __name__ == "__main__":
     
-    pyboy = PyBoy('rom/PokemonRed.gb')
+    pyboy = PyBoy('rom/PokemonRed.gb', game_wrapper=True)
+
+    pyboy.set_emulation_speed(1)
+
+    poke_red = pyboy.game_wrapper()
+    poke_red.start_game()
+
+    print(poke_red.get_player_monster(0))
+
+    with open('game_states/base.state', 'rb') as f:
+        pyboy.load_state(f)
+
+    while not pyboy.tick():
+        pass
+    pyboy.stop()
+
+    # with open('game_states/base.state', 'wb') as f:
+    #     pyboy.save_state(f)
