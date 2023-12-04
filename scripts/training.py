5c5d922e64cf9797c2010f40cf404e05baecc547
SnarkAttack <patrickm@vrlsec.com>
2023-12-04 16:46:11 -0500

    Update training script with tensorboard integration

--- scripts/training.py
+++ scripts/training.py
@@ -1,3 +1,5 @@
+import uuid
+from pathlib import Path
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
 from pokemon_gen_runner.environments import PokemonGen1Env
@@ -5,8 +7,13 @@ from pokemon_gen_runner.environments import PokemonGen1Env
 if __name__ == "__main__":
 
     max_steps = 100
+    num_epochs = 10
 
-    checkpoint_callback = CheckpointCallback(save_freq=max_steps, save_path='output',
+    session_id = str(uuid.uuid4())[:8]
+    session_path = Path(f"output/{session_id}")
+    tensorboard_path = Path(f"tensorboard")
+
+    checkpoint_callback = CheckpointCallback(save_freq=max_steps, save_path=session_path,
                                      name_prefix='pg1')
 
     env_config = {
@@ -17,6 +24,6 @@ if __name__ == "__main__":
 
     env = PokemonGen1Env(env_config)
 
-    model = PPO('MultiInputPolicy', env, n_steps=max_steps, batch_size=10, n_epochs=10, verbose=True)
+    model = PPO('MultiInputPolicy', env, n_steps=max_steps, n_epochs=num_epochs, batch_size=max_steps, verbose=True, tensorboard_log=tensorboard_path)
 
-    model.learn(total_timesteps=max_steps*10, callback=checkpoint_callback)
+    model.learn(total_timesteps=max_steps*num_epochs, callback=checkpoint_callback, tb_log_name=session_id)
