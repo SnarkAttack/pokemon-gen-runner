c84d04386b8728abcb13f77dd28a8e193f562e9a
SnarkAttack <patrickm@vrlsec.com>
2023-12-06 11:51:54 -0500

    Remove non-abstract rewatd tracker from gen1_reward_tracker.py

--- src/pokemon_gen_runner/reward_trackers/gen1_reward_tracker.py
+++ src/pokemon_gen_runner/reward_trackers/gen1_reward_tracker.py
@@ -5,33 +5,9 @@ class Gen1RewardTracker():
     def __init__(self):
         self._total_reward = 0
 
-    def initialize(self, poke_red):
-        raise NotImplementedError(f"initialize not implemented for class {self.__class__.__name__}")
+    @property
+    def total_reward(self):
+        return self._total_reward
 
     def update_reward(self, poke_red):
         raise NotImplementedError(f"update_reward not implemented for class {self.__class__.__name__}")
-    
-
-class FindGrassRewardTracker(Gen1RewardTracker):
-
-    def __init__(self, poke_red):
-        super().__init__()
-        self._location = poke_red.get_player_location()
-
-    def update_reward(self, poke_red):
-        map = poke_red._get_screen_background_tilemap()[::2, ::2]
-        location = poke_red.get_player_location()
-
-        reward = 0
-
-        if location != self._location:
-            reward += 1
-
-        self._location = location
-
-        if map[4,4] == 338:
-            reward += 10
-
-        self._total_reward += reward
-        
-        return reward
